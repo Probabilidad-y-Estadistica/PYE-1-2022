30f55d050594e46f26fdaa95a90ceb3bdda07579
May <maymmiqueiro@gmail.com>
2022-10-06 20:26:27 -0300

    Update Ejercicio2.R

--- Ejercicio2.R
+++ Ejercicio2.R
@@ -18,26 +18,30 @@ d = rexp(10^5, lambda)
 
 # Con boxplot generamos el gráfico de cajas agregado color para distiguir 
 # cada gráfico y borramos el eje "y" para agregar uno más exacto
-boxplot(#values ~ group,
-  #data,
+boxplot(
   a,b,c,d,
-  col = c("blue",
-          "green", 
-          "orange", 
-          "yellow"),
+  col = c("darkseagreen1",
+          "darkseagreen2", 
+          "darkseagreen3", 
+          "darkseagreen4"),
   main = "Distribución Exponencial",
   xlab = "Muestras",
   ylab = "Valor variable aleatoria",
   yaxt='n')
 
-#Mostramos los datos particulares de cada una de las muestras obtenidas.
+# Mostramos los datos particulares de cada una de las muestras obtenidas.
 boxplot.stats(a)
 boxplot.stats(b)
 boxplot.stats(c)
 boxplot.stats(d)
 
 # Agregamos eje "y" mas exacto
-axis(2, at=seq(0, 60, 1),las=2)
+axis(2, at=seq(0, 10, 0.2),las=2)
+
+median(a) # 0.1972078 $conf[1] 0.1610968 0.2333188
+median(b) # 0.2024430 $conf[1] 0.1870399 0.2178462
+median(c) # 0.1995386 $conf[1] 0.1945816 0.2044957
+median(d) # 0.1983504 $conf [1] 0.1967769 0.1999239
 
 ############################## Parte B #######################################
 # Utilizamos "mean" para calcular la esperanza empírica de cada muestra
@@ -80,14 +84,47 @@ comparVD = varD - varT
 
 ## dexp: La función en R para calcular la función de densidad de probabilidad exponencial de λ https://r-coder.com/distribucion-exponencial-r/
 
-# Con n=100 (muestra 10^2)
-muestra102 = rexp(n = 100, rate = lambda)
-hist(muestra102, prob = TRUE, main = "Histograma de densidad", # Densidad
-     ylab = "Densidad")
-lines(density(muestra102), lwd = 2, col = 'red') # densidad empirica ?? https://r-coder.com/grafico-densidad-r/
+# Histogramas
+# Generamos un histograma con los valores obtenidos de realizar los experimentos aleatorios
+# Rejilla del eje X
+x = seq(0, 8, 0.1)
+# Histograma para  muestra 10^2
+hist(a, 
+     main="Distribución Exponencial - muestra 10^2",
+     breaks = 20, 
+     xaxt="n",
+     ylab="Densidad",
+     xlab="x",
+     freq=FALSE)
+# Dibujamos una línea con la función de densidad
+lines(x, dexp(x, rate = 3.5), col = "blue", lty = 1, lwd = 2)
 
-# Con n= 10^5 (muestra 10^5)
-muestra105 = rexp(n = 10^5, rate = lambda)
-hist(muestra105, freq = FALSE, main = "Curva densidad", ylab = "Densidad")
-lines(density(muestra105), lwd = 2, col = 'red')
+# Agregamos una leyenda
+legend("topright", 
+       legend = c("Valores aleatorios",
+                  "Función de densidad"),
+       lty = 1, col = c("grey","blue"), lwd =1, box.lty =1)
+
+# Realizamos el histograma para el tamaño de muestra 10^5 análogamente al 10^2
+# Rejilla del eje X
+x = seq(0, 8, 0.1)
+
+# Histograma para  muestra 10^5
+hist(d, 
+     main="Distribución Exponencial - muestra 10^5",
+     ylab="Densidad",
+     xlab="x",
+     breaks = 50,
+     xaxt="n", 
+     freq=FALSE)
+
+# Dibujamos una línea con la función de densidad
+lines(x, dexp(x, rate = 3.5), col = "blue", lty = 1, lwd = 2)
+
+# Agregamos una leyenda
+legend("topright", 
+       legend = c("Valores aleatorios",
+                  "Función de densidad"),
+       lty = 1, col = c("grey","blue"), lwd =1, box.lty =1)
+
 

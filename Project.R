31e7a62a4aa0ba26d5b39b0f2b59439da1376c1d
astorlucas <lucasastor@gmail.com>
2022-10-06 13:46:06 -0300

    histogramas ready

--- Project.R
+++ Project.R
@@ -119,23 +119,20 @@ legend("bottomright",
 # Distribucion Exponencial(3.5)
 # n = 10 ^ (2 al 5)
 
-# Generamos 4 constantes "a", "b", "c" y "d" aplicando la distribucion normal
-# para los tamaÃ±os de muestra 10^2, 10^3, 10^4 y 10^5 respectivamente
+# Antes de generar las distribuciones fijamos una semilla para que las salidas sean reproducibles
+set.seed(3.5)
+
+# Generamos 4 constantes "a", "b", "c" y "d" aplicando la distribucion exponencial
+# para los tamanos de muestra 10^2, 10^3, 10^4 y 10^5 respectivamente
 a = rexp(10^2, rate = 3.5)
 b = rexp(10^3, rate = 3.5)
 c = rexp(10^4, rate = 3.5)
 d = rexp(10^5, rate = 3.5)
 
-# Para graficar las 4 mustras en un mismo grÃ¡fico de cajas generamos un array
-# con los resultados
-
-#randomValues <- c(a,b,c,d)
-#data <- data.frame(values =randomValues,
-#group = c("10^2","10^3","10^4","10^5"))
 
 # Con boxplot generamos el grÃ¡fico de cajas agregado color para distiguir 
-# cada grÃ¡fico y borramos el eje "y" para agregar uno mÃ¡s exacto, al igual
-# que lo hicimos con la distribuciÃ³n binomial
+# cada grafico y borramos el eje "y" para agregar uno mas exacto, al igual
+# que lo hicimos con la distribucion de poisson
 boxplot(#values ~ group,
   #data,
   a,b,c,d,
@@ -148,33 +145,30 @@ boxplot(#values ~ group,
   ylab = "Valor variable aleatoria", 
   yaxt='n')
 
-# Agregamos eje "y" mÃ¡s exacto
+# Agregamos eje "y" mas exacto
 axis(2, at=seq(-20, 20, 1),las=2)
+
+# Obtenemos los stats de cada boxplot para analizar
 boxplot.stats(a)
 boxplot.stats(b)
 boxplot.stats(c)
 boxplot.stats(d)
 
-# Como estamos trabajando con la dsitribuciÃ³n normal, sabemos que la esperanza
-# estÃ¡ definida por el valor otorgado para los parÃ¡metros de la
-# distribuciÃ³n normal
-espT = -4
+# Como estamos trabajando con la dsitribucion exponencial, sabemos que la esperanza
+# teorica se calcula como 1/lambda o 1/rate
+espT = (1/3.5)
 
-# Al igual que la esperanza, la varianza es la raÃ­z cuadrada de la desviaciÃ³n
-# estandar, en este caso estÃ¡ definida por el valor otorgado 
-# para los parÃ¡metros de la distribuciÃ³n normal
-varT = 16
-
-# Al igual que con la distribuciÃ³n binomial, utilizamos "mean" para calcular
-# la esperanza empirica
+# Se utiliza "mean" para calcular la esperanza empirica
 espA = mean(a)
 espB = mean(b)
 espC = mean(c)
 espD = mean(d)
 
+# La varianza teorica se calcula como 1/lambda^2 en este caso (3.5^2)
+varT = (1/(3.5^2))
+
 # Varianza Empirica
-# Al igual que con la distribuciÃ³n binomial, utilizamos "var" para calcular
-# la varianza teÃ³rica empirica
+# Se utiliza "var" para calcular la varianza empirica
 varA = var(a)
 varB = var(b)
 varC = var(c)
@@ -182,53 +176,52 @@ varD = var(d)
 
 # Histogramas
 # Generamos un histograma con los valores obtenidos de ralizar los experimentos aleatorios
+# Rejilla del eje X
+x <- seq(0, 8, 0.1)
+# Histograma para  muestra 10^2
 hist(a, 
-     main="Distribución normal - muestra 10^2",
+     main="Distribución Exponencial - muestra 10^2",
      breaks = 10, 
      xaxt="n",
      ylab="Densidad",
      xlab="x",
      freq=FALSE)
-# Utilizamos un array de valores del -20 al 10 con saltos de a 1
-x <- seq(-20, 10, 1)
-# Graficamos sobre el histograma la densidad normal con los valores otorgados
-lines(x, 
-      dnorm(x, mean = -4, sd = 4), 
-      col = "blue",
-      lty = 1, 
-      lwd = 2,
-      xaxt="n")
-# Pintamos el area debajo del gráfico por estética
-polygon(x, 
-        dnorm(x, mean = -4, sd = 4), 
-        col = rgb(0, 0, 1, alpha = 0.5))
-# Y agregamos un eje para hacer más claros los saltos y la escala
-axis(1, at=seq(-20, 10, 1), las=2)
+# Dibujamos una linea con la funcion de densidad
+lines(x, dexp(x, rate = 3.5), col = "blue", lty = 1, lwd = 2)
+
+# Agregamos una leyenda
+legend("topright", 
+       legend = c("Valores aleatorios",
+                  "Funcion de densidad"),
+       lty = 1, col = c("grey","blue"), lwd =1, box.lty =1)
 
 
 # Realizamos el histograma para el tamaño de muestra 10^5 analogamente al 10^2
+# Rejilla del eje X
+x <- seq(0, 8, 0.1)
+
+# Histograma para  muestra 10^5
 hist(d, 
-     main="Distribución normal - muestra 10^5",
+     main="Distribución Exponencial - muestra 10^5",
      ylab="Densidad",
      xlab="x",
      breaks = 50,
      xaxt="n", 
      freq=FALSE)
-lines(x, 
-      dnorm(x, mean = -4, sd = 4), 
-      col = "blue", 
-      lty = 1, 
-      lwd = 2, 
-      xaxt="n")
-polygon(x, 
-        dnorm(x, mean = -4, sd = 4), 
-        col = rgb(0, 0, 1, alpha = 0.5))
-axis(1, at=seq(-20, 10, 1),las=1)
+# Dibujamos una linea con la funcion de densidad
+lines(x, dexp(x, rate = 3.5), col = "blue", lty = 1, lwd = 2)
 
-legend("topleft", 
+# Agregamos una leyenda
+legend("topright", 
        legend = c("Valores aleatorios",
-                  "FunciÃ³n de densidad"),
+                  "Funcion de densidad"),
        lty = 1, col = c("grey","blue"), lwd =1, box.lty =1)
+
+# Pintamos debajo de la grafica
+#polygon(x, 
+#        dexp(x, rate = 3.5), 
+#        col = rgb(0, 0, 1, alpha = 0.5))
+#axis(1, at=seq(-20, 10, 1),las=1)
 
 #------------------------------------------------------------------------------------
 
